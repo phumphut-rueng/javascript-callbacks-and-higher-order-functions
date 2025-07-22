dedd642aadc653c43aeedbc538ad8e248360646b
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-07-22 14:00:44 +0700

    refactor: improve callback and higher-order function implementations in exercises 1 and 2

--- ex-2.js
+++ ex-2.js
@@ -4,17 +4,30 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
-  // Start coding here
+  return score > 70;
 }
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let count = 0;
+for(let i=0 ;i<array.length;i++){
+  if(operation(array[i])){
+    count ++
+    }
+  }
+  if(count>=5){
+    return true;
+  }else{
+    return false;
+  }
 }
 
+
+
+
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
@@ -23,9 +36,9 @@ console.log(scoreRoom3Result); //false
 /*
 ====================================
 
-1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function? checkGradeOver70() เป็น Callback
 
-2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function? atLeastFive() เป็น Higher Order
 
 ====================================
 */
